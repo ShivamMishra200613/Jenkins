8e2bbb22848b3b64f96de711af840b47aeb7d773
ShivamMishra200613 <2024.shivam.mishra@ves.ac.in>
2025-09-17 10:12:38 +0530

    Update3 Hello.java

--- Hello.java
+++ Hello.java
@@ -4,8 +4,5 @@ class Hello {
   public static void main(String args[]) {
     System.out.println("Hello World");
     System.out.println("RCB IPL win was fixed");
-    Scanner sc = new Scanner(System.in);
-    String ans = sc.next();
-    System.out.println(ans);
   }
 }
